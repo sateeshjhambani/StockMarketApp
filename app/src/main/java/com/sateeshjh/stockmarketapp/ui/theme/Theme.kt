453607e3a6b24d76db455cfd10a0a96c92045c91
Sateesh <sateesh.jh@gmail.com>
2023-02-02 00:10:51 +0500

    Refactored Dependencies

--- app/src/main/java/com/sateeshjh/stockmarketapp/ui/theme/Theme.kt
+++ app/src/main/java/com/sateeshjh/stockmarketapp/ui/theme/Theme.kt
@@ -5,7 +5,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import com.plcoding.stockmarketapp.ui.theme.*
 
 private val DarkColorPalette = darkColorScheme(
     primary = Purple200,
